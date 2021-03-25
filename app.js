a610db3c6a06f28b4766869d9f07c2a697d2bab2
Burk Manson <27848938+burk96@users.noreply.github.com>
2021-03-24 21:27:35 -0700

    Added food logic, completed main goals

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let gridY = 30;
 let fps = 2;
 let clock;
 let snake;
+let food = [];
+let grow = false;
 
 // initial state
 let gameState = {
@@ -50,7 +52,13 @@ function buildSnake() {
   ];
 
   snake.body.unshift(newSnakeHead);
-  snake.body.pop();
+
+  // By default, grow is false, however eating food will set it to true
+  if (!grow) {
+    snake.body.pop();
+  } else {
+    grow = false;
+  }
 
   snake.body.forEach(function (coordinates) {
     const coordinateX = coordinates[0];
@@ -63,27 +71,46 @@ function buildSnake() {
   });
 }
 
-function dropFood(foodType = "apple") {
-  let foodX = Math.floor(Math.random() * gridX);
-  let foodY = Math.floor(Math.random() * gridY);
-  let food = [foodX, foodY];
+function createFood(foodType = "apple") {
+  let foodX;
+  let foodY;
+  let foodElement;
 
-  const foodElement = $(`[data-x="${foodX}"][data-y="${foodY}"]`);
+  do {
+    foodX = Math.floor(Math.random() * gridX);
+    foodY = Math.floor(Math.random() * gridY);
+
+    foodElement = $(`[data-x="${foodX}"][data-y="${foodY}"]`);
+  } while (foodElement.hasClass("snake"));
   foodElement.addClass(`${foodType}`);
+
+  let foodItem = {
+    coordinates: [foodX, foodY],
+    type: foodType,
+  };
+  food.push(foodItem);
+}
+
+function removeFood(type) {
+  // Finds the index of the Foodtype in the food array
+  let index = food
+    .map(function (food) {
+      return food.type;
+    })
+    .indexOf(type);
+
+  let coordinates = food[index].coordinates;
+
+  let foodElement = $(
+    `[data-x="${coordinates[0]}"][data-y="${coordinates[1]}"]`
+  );
+
+  foodElement.removeClass(type);
+  food.pop(index);
 }
 
 function collisionDetect() {
   const snakeHead = snake.body[0];
-
-  //checks if we go out of bounds
-  if (
-    snakeHead[0] < 0 ||
-    snakeHead[0] >= gridX ||
-    snakeHead[1] < 0 ||
-    snakeHead[1] >= gridY
-  ) {
-    gameOver("Snake left gameboard!");
-  }
 
   //detects if the snake head is touching the body
   snake.body.forEach(function (coordinates, coordinateIndex) {
@@ -92,6 +119,39 @@ function collisionDetect() {
       JSON.stringify(coordinates) == JSON.stringify(snakeHead)
     ) {
       gameOver("Snake ate itself!");
+    }
+  });
+}
+
+function outOfBoundsDetect() {
+  const snakeHead = snake.body[0];
+
+  if (
+    snakeHead[0] < 0 ||
+    snakeHead[0] >= gridX ||
+    snakeHead[1] < 0 ||
+    snakeHead[1] >= gridY
+  ) {
+    gameOver("Snake left gameboard!");
+  }
+}
+
+function foodCheck() {
+  // Checks to see if the snake has eaten food
+  // Loops through food array and will check type if collision
+  const snakeHead = snake.body[0];
+
+  food.forEach(function (food) {
+    if (JSON.stringify(food.coordinates) === JSON.stringify(snakeHead)) {
+      switch (food.type) {
+        case "apple":
+          removeFood("apple");
+          createFood("apple");
+          grow = true;
+          break;
+        default:
+          break;
+      }
     }
   });
 }
@@ -119,17 +179,19 @@ function buildInitialState() {
   resetSnake();
   renderState();
   buildSnake();
-  dropFood();
+  createFood();
 }
 
 // refresh the screen in an interval
 function tick() {
   buildSnake();
   collisionDetect();
+  outOfBoundsDetect();
+  foodCheck();
 }
 
 function startGame() {
-  clock = setInterval(tick, 1000 / fps); //default is 30
+  clock = setInterval(tick, 1000 / fps);
   $(".start-button").text("Stop");
   $(".start-button").css({ "background-color": "red" });
   gameState.isRunning = true;
